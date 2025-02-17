43b1d5aa7b3e8a244ff4b32eaa18dd222a80695f
Jose1-0 <josealedrr@gmail.com>
2025-02-17 16:24:48 -0300

    criacao do projeto v2, adicao de melhorias

---
+++ src/test/java/com/br/AplicacaoFinanceira/AppFinanceiro/AppFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.AplicacaoFinanceira.AppFinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
